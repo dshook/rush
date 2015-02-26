d38797b61d78a4163ce284befc0cb01fa4a31779
Dillon Shook <dshook@weblinc.com>
2015-02-26 16:41:04 -0500

    tabs to spaces

--- app/client/activities/HomeActivity.js
+++ app/client/activities/HomeActivity.js
@@ -3,20 +3,20 @@ import layout from '../views/Layout.jsx';
 
 export default class HomeActivity
 {
-	constructor($root, stores){
-		this.$root  = $root;
-		this.stores = stores;
-	}
+  constructor($root, stores){
+    this.$root  = $root;
+    this.stores = stores;
+  }
 
-	onStart(){
-		var Layout = React.createFactory(layout);
-	  //init layout with all deps passed down
-	  React.render(
-	    new Layout({
-	      stores: this.stores 
-	    })
-	    , this.$root[0]
-	  );
-	}
+  onStart(){
+    var Layout = React.createFactory(layout);
+    //init layout with all deps passed down
+    React.render(
+      new Layout({
+        stores: this.stores 
+      })
+      , this.$root[0]
+    );
+  }
 }
 
