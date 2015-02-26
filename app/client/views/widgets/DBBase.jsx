93bb98801e53c56a661bb82368ad0639f9f29700
Dillon Shook <dshook@weblinc.com>
2015-02-26 16:20:56 -0500

    Fixed connection saving, made a base component for db drivers

---
+++ app/client/views/widgets/DBBase.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import WidgetActions from '../../actions/WidgetActions.js';
+import forms from 'newforms';
+import ConnectionForm from './forms/Connection.jsx';
+import QueryForm from './forms/Query.jsx';
+
+export default class DBBase extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = props.widget;
+
+    this.saveWidget = this.saveWidget.bind(this);
+  }
+
+  saveWidget(e){
+    e.preventDefault();
+
+    var connectionForm = this.refs.connectionForm.getForm();
+    var queryForm = this.refs.queryForm.getForm() ;
+    if(connectionForm.validate() && queryForm.validate()){
+      var data = connectionForm.cleanedData;
+      var query = queryForm.cleanedData.query;
+
+      this.setState({config: data, query}, function(){
+        WidgetActions.updateWidget(this.state);
+        this.props.onClose();
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className="modal-widget">
+        <h3>{this.state.name}</h3>
+        <form onSubmit={this.saveWidget}>
+          <forms.RenderForm 
+            initial={this.state.config} 
+            form={ConnectionForm} 
+            ref="connectionForm" />
+          <forms.RenderForm 
+            initial={{query: this.state.query}} 
+            form={QueryForm} 
+            ref="queryForm" />
+          <button type="submit" className="button button--action" >
+            <i className="fa fa-save"></i>
+            Save
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
--- app/client/views/widgets/SqlServer.jsx
+++ app/client/views/widgets/SqlServer.jsx
@@ -1,51 +1,7 @@
-import React from 'react';
-import WidgetActions from '../../actions/WidgetActions.js';
-import forms from 'newforms';
-import ConnectionForm from './forms/Connection.jsx';
-import QueryForm from './forms/Query.jsx';
+import DBBase from './DBBase.jsx';
 
-export default class SqlServer extends React.Component {
+export default class SqlServer extends DBBase {
   constructor(props){
     super(props);
-    this.state = props.widget;
-
-    this.saveWidget = this.saveWidget.bind(this);
-  }
-
-  saveWidget(e){
-    e.preventDefault();
-
-    var form = this.refs.connectionForm.getForm();
-    var data = form.cleanedData;
-
-    var query = this.refs.queryForm.getForm().cleanedData.query;
-
-    this.setState({config: data, query}, function(){
-      WidgetActions.updateWidget(this.state);
-      this.props.onClose();
-    });
-
-  }
-
-  render() {
-    return (
-      <div className="modal-widget">
-        <h3>{this.props.name}</h3>
-        <form onSubmit={this.saveWidget}>
-          <forms.RenderForm 
-            initial={this.state.config} 
-            form={ConnectionForm} 
-            ref="connectionForm" />
-          <forms.RenderForm 
-            initial={{query: this.state.query}} 
-            form={QueryForm} 
-            ref="queryForm" />
-          <button type="submit" className="button button--action" >
-            <i className="fa fa-save"></i>
-            Save
-          </button>
-        </form>
-      </div>
-    );
   }
 }
