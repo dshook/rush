1ab9979b3a84b0ae30140ac7814203f1080e7746
Dillon Shook <dshook@alumni.nmt.edu>
2015-03-16 21:30:42 -0400

    App.make is the way to go

--- app/client/stores/Stores.js
+++ app/client/stores/Stores.js
@@ -2,12 +2,11 @@ import {WidgetStore} from'./WidgetStore.js';
 import {AppStore} from'./AppStore.js';
 import {WidgetProviderStore} from'./WidgetProviderStore.js';
 
-export default function Stores(app, transport, config)
+export default function Stores(app)
 {
-  //TODO: app.make eh?
   app.register('stores', {
-    widgetStore: new WidgetStore(transport),
-    appStore: new AppStore(transport),
-    widgetProviderStore: new WidgetProviderStore(transport)
+    widgetStore: app.make(WidgetStore),
+    appStore: app.make(AppStore),
+    widgetProviderStore: app.make(WidgetProviderStore)
   }).asInstance();
 }
